e899764e71c64aad3b3f44499df6e11c1a98931c
perfumelim <perfumelim@gmail.com>
2022-05-26 21:35:16 +0900

    ✨ slug 구현 및 comment data fetching

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,5 +1,95 @@
-const ArticleDetail = () => {
-  return <h1>Test</h1>;
+import type { NextPage, GetStaticProps, GetStaticPaths } from "next";
+import Link from "next/link";
+
+type ArticleT = {
+  slug: string;
+  title: string;
+  description: string;
+  body: string;
+  tagList: string[];
+  createdAt: string;
+  updatedAt: string;
+  favorited: boolean;
+  favoritesCount: number;
+  author: ProfileT;
+};
+
+type CommentT = {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  body: string;
+  author: ProfileT;
+};
+
+type ProfileT = {
+  bio: string;
+  following: boolean;
+  image: string;
+  username: string;
+};
+
+type MultipleCommentResponse = {
+  comments: CommentT[];
+};
+
+interface ArticleDetailProps {
+  article: ArticleT;
+  comments: CommentT[];
+}
+
+const ArticleDetail: NextPage<ArticleDetailProps> = ({ article, comments }) => {
+  return (
+    <>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <h1>{article.title}</h1>
+      <p> {article.body}</p>
+
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}>
+            <h3>{comment.author.username}</h3>
+            <p>{comment.body}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default ArticleDetail;
+
+type ArticleDetailParams = {
+  slug: string;
+};
+
+export const getStaticPaths: GetStaticPaths<ArticleDetailParams> = async () => {
+  return {
+    paths: [
+      { params: { slug: "Create-a-new-implementation-1" } },
+      { params: { slug: "Explore-implementations-1" } },
+      { params: { slug: "Welcome-to-RealWorld-project-1" } },
+    ],
+    fallback: false, // false or 'blocking'
+  };
+};
+
+export const getStaticProps: GetStaticProps<
+  ArticleDetailProps,
+  ArticleDetailParams
+> = async (context) => {
+  const { slug } = context.params!;
+
+  const { article } = await fetch(
+    `https://api.realworld.io/api/articles/${slug}`
+  ).then((res) => res.json());
+  const { comments } = await fetch(
+    `https://api.realworld.io/api/articles/${slug}/comments`
+  ).then((res) => res.json());
+
+  return {
+    props: { article, comments },
+  };
+};
